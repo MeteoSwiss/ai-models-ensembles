de71ae4998bea369a4081d46f3e4ced46c7657aa
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-11-29 12:35:44 +0100

    improved plotting with unperturbed member

--- evaluation.py
+++ evaluation.py
@@ -1,24 +1,41 @@
 import argparse
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
 
 parser = argparse.ArgumentParser(description="Evaluate the NeurWP Ensemble.")
-parser.add_argument("path_out", type=str, help="The path to the output directory")
-
+parser.add_argument("model_name", type=str, help="The ai-model name")
+parser.add_argument(
+    "date_time",
+    type=str,
+    help="Date and time in the format YYYYMMDDHHMM")
+parser.add_argument("perturbation", type=float, help="The perturbation size")
 args = parser.parse_args()
 
-forecast = xr.open_zarr(args.path_out + "/forecast.zarr", consolidated=True)
+path_out = os.path.join(
+    args.model_name,
+    str(args.date_time),
+    str(args.perturbation))
+
+forecast = xr.open_zarr(path_out + "/forecast.zarr", consolidated=True)
+forecast_unperturbed = xr.open_zarr(
+    args.model_name +
+    "/forecast.zarr",
+    consolidated=True)
 ground_truth = xr.open_zarr("ground_truth.zarr", consolidated=True)
 
 ground_truth['step'] = ('time', np.arange(len(ground_truth['time'])))
 ground_truth = ground_truth.swap_dims({'time': 'step'})
 forecast["step"] = forecast.time + forecast.step
+forecast_unperturbed["step"] = forecast["step"]
 ground_truth["step"] = forecast["step"]
 
 # Calculate the spatial mean
 fc_mean = forecast.mean(dim=["latitude", "longitude", "isobaricInhPa"])
+fc_mean_unperturbed = forecast_unperturbed.mean(
+    dim=["latitude", "longitude", "isobaricInhPa"])
 gt_mean = ground_truth.mean(dim=["latitude", "longitude", "isobaricInhPa"])
 
 # Calculate the std across all members for each forecast step and variable
@@ -47,13 +64,11 @@ for variable in spread_skill_ratio.data_vars:
     ax.set_ylim(bottom=0)
     ax.set_title(f"Spread-Skill Ratio and Ensemble Spread for {variable}")
     ax2.set_ylabel("Ensemble Spread", color='red')
-    ax2.tick_params(axis='y', colors='red')  # Make the right axis and its labels red
+    ax2.tick_params(axis='y', colors='red')
     ax2.set_title("")
-    plt.savefig(f"{args.path_out}/spread_skill_ratio_{variable}.png")
+    plt.savefig(f"{path_out}/spread_skill_ratio_{variable}.png")
     plt.close(fig)
 
-# Plot the gt_mean and each member of fc_mean for each variable
-for variable in fc_mean.data_vars:
     fig, ax = plt.subplots(figsize=(8, 6))
 
     # Plot gt_mean in black
@@ -66,14 +81,21 @@ for variable in fc_mean.data_vars:
                 member=member).plot(
                 ax=ax,
                 color='green',
-                alpha=0.5,
+                alpha=0.3,
                 label='AI-Model')
         else:
-            fc_mean[variable].sel(member=member).plot(ax=ax, color='green', alpha=0.5)
+            fc_mean[variable].sel(member=member).plot(ax=ax, color='green', alpha=0.05)
+    fc_mean[variable].median(
+        dim="member").plot(
+        ax=ax,
+        color='orange',
+        label='AI-Model Median')
+    fc_mean_unperturbed[variable].plot(
+        ax=ax, color='purple', label='AI-Model Unperturbed')
 
     ax.set_ylabel(f"{variable}")
     ax.set_title(f"Forecast vs Ground-Truth Comparison: {variable}")
     ax.legend()
 
-    plt.savefig(f"{args.path_out}/{variable}_fc_gt.png")
+    plt.savefig(f"{path_out}/{variable}_fc_gt.png")
     plt.close(fig)
