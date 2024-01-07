37d778ccc5ba2ee7de5dcf1d6d11468ad9d18ff7
Simon Adamov <simon.adamov@meteoswiss.ch>
2024-01-07 10:56:55 +0100

    addition of simple rmse plots over time to observe error growth

--- evaluation.py
+++ evaluation.py
@@ -6,25 +6,27 @@ import numpy as np
 import xarray as xr
 
 parser = argparse.ArgumentParser(description="Evaluate the NeurWP Ensemble.")
-parser.add_argument("model_name", type=str, help="The ai-model name")
 parser.add_argument(
     "date_time",
     type=str,
     help="Date and time in the format YYYYMMDDHHMM")
-parser.add_argument("perturbation", type=float, help="The perturbation size")
+parser.add_argument("model_name", type=str, help="The ai-model name")
+parser.add_argument("perturbation_init", type=float, help="The init perturbation size")
+parser.add_argument(
+    "perturbation_latent",
+    type=float,
+    help="The latent perturbation size")
 args = parser.parse_args()
 
 path_out = os.path.join(
-    args.model_name,
     str(args.date_time),
-    str(args.perturbation))
+    args.model_name,
+    f"init_{args.perturbation_init}_latent_{args.perturbation_latent}")
 
 forecast = xr.open_zarr(path_out + "/forecast.zarr", consolidated=True)
 forecast_unperturbed = xr.open_zarr(
-    args.model_name +
-    "/forecast.zarr",
-    consolidated=True)
-ground_truth = xr.open_zarr("ground_truth.zarr", consolidated=True)
+    f"{args.date_time}/{args.model_name}/forecast.zarr", consolidated=True)
+ground_truth = xr.open_zarr(args.date_time + "/ground_truth.zarr", consolidated=True)
 
 ground_truth['step'] = ('time', np.arange(len(ground_truth['time'])))
 ground_truth = ground_truth.swap_dims({'time': 'step'})
@@ -38,26 +40,59 @@ fc_mean_unperturbed = forecast_unperturbed.mean(
     dim=["latitude", "longitude", "isobaricInhPa"])
 gt_mean = ground_truth.mean(dim=["latitude", "longitude", "isobaricInhPa"])
 
-# Calculate the std across all members for each forecast step and variable
-ensemble_spread = fc_mean.std(dim=["member"])
+# Calculate the squared differences at each spatial point
+squared_diff = (forecast - ground_truth) ** 2
+squared_diff_unperturbed = (forecast_unperturbed - ground_truth) ** 2
 
-# Calculate the ensemble skill for each forecast step and variable
-ensemble_skill = abs(fc_mean.mean(dim="member") - gt_mean)
+# Calculate the spatial mean of the squared differences
+mean_squared_diff = squared_diff.mean(dim=["latitude", "longitude", "isobaricInhPa"])
+mean_squared_diff_unperturbed = squared_diff_unperturbed.mean(
+    dim=["latitude", "longitude", "isobaricInhPa"])
 
-# Calculate the spread and skill at each forecast step
+# Calculate the RMSE
+rmse = np.sqrt(mean_squared_diff)
+rmse_unperturbed = np.sqrt(mean_squared_diff_unperturbed)
+
+ensemble_spread = rmse.std(dim=["member"])
+ensemble_skill = abs(rmse.mean(dim="member"))
 spread_skill_ratio = ensemble_spread / ensemble_skill
+
+alpha_value = 1 / rmse.member.size**(2 / 3)
 
 # Plot the spread and skill at each forecast step and variable
 for variable in spread_skill_ratio.data_vars:
     print("Creating plots for variable: ", variable)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Plot each member's rmse in green
+    for i, member in enumerate(rmse.member):
+        rmse_member_mean = rmse[variable].sel(member=member)
+        if i == 0:  # Add label only for the first member
+            rmse_member_mean.plot(
+                ax=ax,
+                color='green',
+                alpha=alpha_value,
+                label='AI-Model Members')
+        else:
+            rmse_member_mean.plot(ax=ax, color='green', alpha=alpha_value)
+
+    # Plot the unperturbed rmse in purple
+    rmse_unperturbed[variable].plot(
+        ax=ax, color='purple', label='AI-Model Unperturbed')
+
+    ax.set_ylabel(f"{variable}")
+    ax.set_title(f"Mean Absolute Global Forecast Error: {variable}")
+    ax.legend()
+    plt.savefig(f"{path_out}/rmse_{variable}.png")
+    plt.close(fig)
+
     data_array = spread_skill_ratio[variable]
     fig, ax = plt.subplots(figsize=(8, 6))
-    # Assuming 'step' is a DataArray or Dataset of numpy.datetime64 objects
     data_array.plot(ax=ax)
 
     # Create a secondary axis
     ax2 = ax.twinx()
-    # Plot the ensemble skill on the secondary axis
     ensemble_spread[variable].plot(ax=ax2, color='red')
 
     ax.set_xlabel("")
@@ -71,10 +106,7 @@ for variable in spread_skill_ratio.data_vars:
     plt.close(fig)
 
     fig, ax = plt.subplots(figsize=(8, 6))
-
-    # Plot gt_mean in black
     gt_mean[variable].plot(ax=ax, color='black', label='ERA5')
-
     # Plot each member of fc_mean in green
     for i, member in enumerate(fc_mean.member):
         if i == 0:  # Add label only for the first member
@@ -82,10 +114,14 @@ for variable in spread_skill_ratio.data_vars:
                 member=member).plot(
                 ax=ax,
                 color='green',
-                alpha=0.3,
-                label='AI-Model')
+                alpha=alpha_value,
+                label='AI-Model Members')
         else:
-            fc_mean[variable].sel(member=member).plot(ax=ax, color='green', alpha=0.05)
+            fc_mean[variable].sel(
+                member=member).plot(
+                ax=ax,
+                color='green',
+                alpha=alpha_value)
     fc_mean[variable].median(
         dim="member").plot(
         ax=ax,
@@ -97,6 +133,5 @@ for variable in spread_skill_ratio.data_vars:
     ax.set_ylabel(f"{variable}")
     ax.set_title(f"Forecast vs Ground-Truth Comparison: {variable}")
     ax.legend()
-
-    plt.savefig(f"{path_out}/{variable}_fc_gt.png")
+    plt.savefig(f"{path_out}/timeseries_fc_gt_{variable}.png")
     plt.close(fig)
