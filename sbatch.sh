ade4f7ab87647413be7d595b4745817d3d78a76d
Simon Adamov <simon.adamov@meteoswiss.ch>
2024-01-15 10:09:46 +0100

    changed date / model order no requeue more memory

--- sbatch.sh
+++ sbatch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -l
 #SBATCH --job-name=ai-models-ensembles
 #SBATCH --nodes=1
-#SBATCH --partition=a100-80gb
+#SBATCH --partition=normal
 #SBATCH --account=s83
 #SBATCH --output=logs.out
 #SBATCH --error=logs.err
 #SBATCH --time=23:59:00
+#SBATCH --no-requeue
+#SBATCH --mem=490G
 
-export MODEL_NAME=fourcastnetv2-small
 export DATE_TIME=201801010000 # Lothar: 199912250000, Burglind: 201801010000
+export MODEL_NAME=fourcastnetv2-small
 export PERTURBATION_INIT=0.0
 export PERTURBATION_LATENT=0.01
 export NUM_MEMBERS=100
